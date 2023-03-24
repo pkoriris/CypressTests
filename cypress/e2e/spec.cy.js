044a01ed7e86c385b720e90470aa4c2b21b6a028
pkoriris <kipkoririsco4@gmail.com>
2023-03-24 22:14:12 +0300

    modify specs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,11 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  describe('My Second Test', () => {
+    it('clicks the link "type"', () => {
+      cy.visit('https://example.cypress.io')
+  
+      cy.contains('type').click()
+    })
+  })
 })
