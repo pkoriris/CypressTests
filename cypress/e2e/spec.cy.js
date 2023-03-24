244a759e662fd4b4c8c9ebccedcdce03cdc5e059
pkoriris <kipkoririsco4@gmail.com>
2023-03-24 22:30:06 +0300

    updating specs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,4 +9,15 @@ describe('empty spec', () => {
       cy.contains('type').click()
     })
   })
+  describe('My First Test', () => {
+    it('clicking "type" navigates to a new url', () => {
+      cy.visit('https://example.cypress.io')
+  
+      cy.contains('type').click()
+  
+      // Should be on a new URL which
+      // includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+    })
+  })
 })
